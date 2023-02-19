678a20ac9a7769e723f844c2ca654789483b7b4d
arthurdiasdeveloper <arthurdiascorporativo@gmail.com>
2023-02-19 12:52:37 -0300

    I inserted some filds

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,24 @@
 import { Header } from '../../components/Header';
+import background from '../../assets/background.png';
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
 
     <Header/>
+      <div className='conteudo'>
+
+        <img src={background} className="background" alt="background App"/>
+
+            <div className='info'>
+                <div>
+                  <input name='usuario' placeholder='@username' />
+                </div>
+
+             </div>
+
+       </div>
 
     </div>
   );
